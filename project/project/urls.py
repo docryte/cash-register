673dd8596c0068a334c3d7f57a81f391407d4fa0
docryte <docryte@gmail.com>
2025-03-17 17:31:47 +0300

    Добавлена статика

--- project/project/urls.py
+++ project/project/urls.py
@@ -4,6 +4,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,7 +30,6 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("", include("store.urls")),
+    *staticfiles_urlpatterns(),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
